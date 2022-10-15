6ce933d092804cf619daee4758d41244e63995fb
licency <69596770+licency@users.noreply.github.com>
2022-10-15 15:45:22 +0530

    Create cheader.h

---
+++ CUT/Code/header/cheader.h
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<signal.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#incude<pthread.h>
+
+#define LENGTH 2048
