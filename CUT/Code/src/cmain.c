5dd6a0566e393e020e8d12bac57435b7ef88948b
lxmknt1 <115132495+lxmknt1@users.noreply.github.com>
2022-10-16 19:43:05 +0530

    Update cmain.c

--- CUT/Code/src/cmain.c
+++ CUT/Code/src/cmain.c
@@ -13,13 +13,13 @@ void str_overwrite_stdout() {
 }
 
 void str_trim_lf (char* arr, int length) {
-  int i;
-  for (i = 0; i < length; i++) { // trim \n
-    if (arr[i] == '\n') {
-      arr[i] = '\0';
-      break;
-    }
-  }
+  	int i;
+  	for (i = 0; i < length; i++) { // trim \n
+    		if (arr[i] == '\n') {
+      		arr[i] = '\0';
+      		break;
+    		}
+  	}
 }
 
 void catch_ctrl_c_and_exit(int sig) {
@@ -29,38 +29,27 @@ void catch_ctrl_c_and_exit(int sig) {
 
 
 void send_msg_handler() {
-  char message[LENGTH] = {};
+	char message[LENGTH] = {};
 	char buffer[LENGTH + 32] = {};
 
-////////////////////adding func for verification////////
 	send(sockfd,"client connected\n",20,0);
 
+  	while(1) {
+  		str_overwrite_stdout();
+    		fgets(message, LENGTH, stdin);
+    		str_trim_lf(message, LENGTH);
 
-
-
-
-
-
-
-
-
-
-
-  while(1) {
-  	str_overwrite_stdout();
-    fgets(message, LENGTH, stdin);
-    str_trim_lf(message, LENGTH);
-
-    if (strcmp(message, "exit") == 0) {
+    		if (strcmp(message, "exit") == 0) {
 			break;
-    } else {
-      sprintf(buffer, "%s: %s\n", name, message);
-      send(sockfd, buffer, strlen(buffer), 0);
-    }
+    		} 
+		else {
+      			sprintf(buffer, "%s: %s\n", name, message);
+      			send(sockfd, buffer, strlen(buffer), 0);
+    			}
 
 		bzero(message, LENGTH);
-    bzero(buffer, LENGTH + 32);
-  }
+    		bzero(buffer, LENGTH + 32);
+  	}
   catch_ctrl_c_and_exit(2);
 }
 
@@ -72,18 +61,16 @@ void send_msg_handler() {
 
 void recv_msg_handler() {
 	char message[LENGTH] = {};
-  while (1) {
+  	while (1) {
 		int receive = recv(sockfd, message, LENGTH, 0);
-    if (receive > 0) {
-      printf("%s", message);
-      str_overwrite_stdout();
-    } else if (receive == 0) {
+    		if (receive > 0) {
+      			printf("%s", message);
+      			str_overwrite_stdout();
+    		} else if (receive == 0) {
 			break;
-    } else {
-			// -1
-		}
+    		}
 		memset(message, 0, sizeof(message));
-  }
+  	}
 }
 
 int main(int argc, char **argv){
@@ -98,8 +85,8 @@ int main(int argc, char **argv){
 	signal(SIGINT, catch_ctrl_c_and_exit);
 
 	printf("Please enter your name: ");
-  fgets(name, 32, stdin);
-  str_trim_lf(name, strlen(name));
+  	fgets(name, 32, stdin);
+  	str_trim_lf(name, strlen(name));
 
 
 	if (strlen(name) > 32 || strlen(name) < 2){
@@ -111,14 +98,14 @@ int main(int argc, char **argv){
 
 	/* Socket settings */
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-  server_addr.sin_family = AF_INET;
-  server_addr.sin_addr.s_addr = inet_addr(ip);
-  server_addr.sin_port = htons(port);
+  	server_addr.sin_family = AF_INET;
+  	server_addr.sin_addr.s_addr = inet_addr(ip);
+  	server_addr.sin_port = htons(port);
 
 
   // Connect to Server
-  int err = connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr));
-  if (err == -1) {
+  	int err = connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr));
+  	if (err == -1) {
 		printf("ERROR: connect\n");
 		return EXIT_FAILURE;
 	}
@@ -129,13 +116,14 @@ int main(int argc, char **argv){
 	printf("=== WELCOME TO THE CHATROOM ===\n");
 
 	pthread_t send_msg_thread;
-  if(pthread_create(&send_msg_thread, NULL, (void *) send_msg_handler, NULL) != 0){
+  	if(pthread_create(&send_msg_thread, NULL, (void *) send_msg_handler, NULL) != 0){
 		printf("ERROR: pthread\n");
-    return EXIT_FAILURE;
+    	
+		return EXIT_FAILURE;
 	}
 
 	pthread_t recv_msg_thread;
-  if(pthread_create(&recv_msg_thread, NULL, (void *) recv_msg_handler, NULL) != 0){
+  	if(pthread_create(&recv_msg_thread, NULL, (void *) recv_msg_handler, NULL) != 0){
 		printf("ERROR: pthread\n");
 		return EXIT_FAILURE;
 	}
@@ -144,8 +132,8 @@ int main(int argc, char **argv){
 		if(flag){
 			printf("\nBye\n");
 			break;
-    }
-	}
+    			}
+		}
 
 	close(sockfd);
 
