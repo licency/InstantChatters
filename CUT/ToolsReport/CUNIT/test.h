475f210d5fc83393018b4ce80ea2bd0e24ee02cc
licency <69596770+licency@users.noreply.github.com>
2022-10-18 20:23:46 +0530

    Add files via upload

---
+++ CUT/ToolsReport/CUNIT/test.h
@@ -0,0 +1,8 @@
+#ifndef TEST_H
+#define TEST_H
+
+// function call
+int validName(char name[]);
+int validPass(char pass[]);
+
+#endif
---
+++ CUT/ToolsReport/CUNIT/testsuite.h
@@ -0,0 +1,20 @@
+#ifndef TESTHEAD
+#define TESTHEAD
+
+#include "CUnit/Basic.h"
+
+#include "test.h"
+
+int init_suite_func1(void);
+int clean_suite_func1(void);
+
+int init_suite_func2(void);
+int clean_suite_func2(void);
+
+void testSunnyCases1(void);
+void testRainyCases1(void);
+
+void testSunnyCases2(void);
+void testRainyCases2(void);
+
+#endif
