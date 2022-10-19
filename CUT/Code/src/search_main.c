b6fc6ec5cdb3f0e6dc43197bfbf23ca2a8b02dcc
lxmknt1 <115132495+lxmknt1@users.noreply.github.com>
2022-10-20 00:18:05 +0530

    Update search_main.c

--- CUT/Code/src/search_main.c
+++ CUT/Code/src/search_main.c
@@ -3,11 +3,11 @@
 #include<string.h>
 
 
-
+/*searches for string, taken from user, in chat history file. */
 int main()
 {
 	FILE *fp;
-int flag=0;
+	int flag=0;
 	char strToBeSearched[100],  str[100];
 	fp = fopen("../data/logfile.txt","r");
 	if(fp ==NULL) printf("Error in opening file \n");
@@ -22,33 +22,20 @@ int flag=0;
 
 		if(fgets(str,100, fp)==NULL)
 		{
-//			printf("Inside if condition \n ");
-//			printf("%s \n",str);
 			break;
 		}
 		else {
-
-
-//			printf("Inside else condition str = %s   \n",str);
-
-			//char *token = strtok(str,":");
-
-			//printf("token =  %s\n",token);
-
 			if(strstr(str,strToBeSearched))
-			{
-			printf("Found in this line %s \n ",str);
-				printf("Written by   %s ",strtok(str,":"));
-
-				flag=1;
-				
-			}
+				{
+					printf("Found in this line %s \n ",str);
+					printf("Written by   %s ",strtok(str,":"));
+					flag=1;
+				}
 		}
 
 	}
-if(flag==0)
+	if(flag==0)
 	printf("Not found in chat history \n");
 	
 return 0;
 }
-
